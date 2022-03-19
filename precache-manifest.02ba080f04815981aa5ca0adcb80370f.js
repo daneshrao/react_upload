9a12430c46cff0e1fa3ae6913e80a02175c379fc
daneshrao <daneshrao@gmail.com>
2022-03-19 21:37:21 +0530

    Updates

---
+++ precache-manifest.02ba080f04815981aa5ca0adcb80370f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3067d4195f68cfe1f5b67245298642e6",
+    "url": "/react_upload/index.html"
+  },
+  {
+    "revision": "846cee00a9b79919c843",
+    "url": "/react_upload/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "c2117e0627a4df98ecf1",
+    "url": "/react_upload/static/css/main.ff94ccf7.chunk.css"
+  },
+  {
+    "revision": "846cee00a9b79919c843",
+    "url": "/react_upload/static/js/2.9e3b7eb1.chunk.js"
+  },
+  {
+    "revision": "41e0b5cee836c131efe4",
+    "url": "/react_upload/static/js/3.6c37e145.chunk.js"
+  },
+  {
+    "revision": "c2117e0627a4df98ecf1",
+    "url": "/react_upload/static/js/main.3ced5185.chunk.js"
+  },
+  {
+    "revision": "d45a7371b62f6beecfb6",
+    "url": "/react_upload/static/js/runtime~main.ffe314a0.js"
+  },
+  {
+    "revision": "6b1e4c7927670880df72bc404cce0c2e",
+    "url": "/react_upload/static/media/Helm.6b1e4c79.png"
+  },
+  {
+    "revision": "9c9b288a875eb33cfa888706b7defc00",
+    "url": "/react_upload/static/media/Hero.9c9b288a.png"
+  },
+  {
+    "revision": "4cec26a61a9af659bc55edbe63d8dd0e",
+    "url": "/react_upload/static/media/about_img.4cec26a6.jpeg"
+  },
+  {
+    "revision": "c7401f520e568a2994f0269e6c537f1d",
+    "url": "/react_upload/static/media/aws.c7401f52.png"
+  },
+  {
+    "revision": "c64430b2948cadf3e67a1a01b8fb1f78",
+    "url": "/react_upload/static/media/css-icon.c64430b2.svg"
+  },
+  {
+    "revision": "10fa1a7127be1df5c1195ef09bd17386",
+    "url": "/react_upload/static/media/elastic.10fa1a71.svg"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/react_upload/static/media/github.d22ee372.png"
+  },
+  {
+    "revision": "c8eba040bbcbe81677312acc9babd6f5",
+    "url": "/react_upload/static/media/grafana.c8eba040.svg"
+  },
+  {
+    "revision": "c83aaf3930796623cdbf82ca002d7005",
+    "url": "/react_upload/static/media/html-icon.c83aaf39.svg"
+  },
+  {
+    "revision": "9dcc3e2b39ac835dd40efb53f50a165c",
+    "url": "/react_upload/static/media/icons8-docker.9dcc3e2b.svg"
+  },
+  {
+    "revision": "5d68edbbac4d19df00b4cd36fc625e4b",
+    "url": "/react_upload/static/media/infy_logo.5d68edbb.jpeg"
+  },
+  {
+    "revision": "70365e83cd76257bcb81174ebc7c7c1a",
+    "url": "/react_upload/static/media/jira.70365e83.svg"
+  },
+  {
+    "revision": "dc2fc5be0984077b3b0fce8c0b8f045a",
+    "url": "/react_upload/static/media/js-icon.dc2fc5be.svg"
+  },
+  {
+    "revision": "337e7181d49cdeebfaf881f59b0c7732",
+    "url": "/react_upload/static/media/kube.337e7181.png"
+  },
+  {
+    "revision": "832e839dbf40a47455ce001ffef770c4",
+    "url": "/react_upload/static/media/prometheus.832e839d.svg"
+  },
+  {
+    "revision": "36972298f00e75c4f7eabe5d81883695",
+    "url": "/react_upload/static/media/python.36972298.svg"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/react_upload/static/media/react-icon.f0793d9f.svg"
+  },
+  {
+    "revision": "3a69682dec932a58869ff54403232195",
+    "url": "/react_upload/static/media/vscode-icon.3a69682d.svg"
+  }
+]);
